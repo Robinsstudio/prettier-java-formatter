d19f74a25addd66a155aab2c2312913bca403de3
Robinsstudio <robin.dosanjos@akur8.com>
2024-08-24 12:25:53 +0200

    Formatter working after command has been run once

--- extension.js
+++ extension.js
@@ -1,5 +1,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+
+// const vscode = require('vscode');
+// const prettier = require('prettier/standalone');
+// const prettierPluginJava = require('prettier-plugin-java');
+
 const vscode = require('vscode');
 
 // This method is called when your extension is activated
@@ -24,11 +29,55 @@ function activate(context) {
 		vscode.window.showInformationMessage('Hello World from prettier-java-formatter!');
 	});
 
+	const command = vscode.commands.registerCommand('prettier-java-formatter.format', async () => {
+		const { activeTextEditor } = vscode.window;
+
+		if (activeTextEditor && activeTextEditor.document.languageId === 'java') {
+			const { document } = activeTextEditor;
+
+			const prettier = await import('prettier/standalone');
+			const prettierPluginJava = await import('prettier-plugin-java');
+
+			const formattedText = await prettier.format(document.getText(), {
+				parser: 'java',
+				plugins: [prettierPluginJava.default],
+			});
+
+			const start = document.positionAt(0);
+			const end = document.positionAt(document.getText().length - 1);
+
+			console.log(formattedText);
+			console.log('Formatted!');
+
+			return [vscode.TextEdit.replace(new vscode.Range(start, end), formattedText)];
+		}
+	});
+
+	const formatter = vscode.languages.registerDocumentFormattingEditProvider('java', {
+		async provideDocumentFormattingEdits(document) {
+			const prettier = await import('prettier/standalone');
+			const prettierPluginJava = await import('prettier-plugin-java');
+
+			const formattedText = await prettier.format(document.getText(), {
+				parser: 'java',
+				plugins: [prettierPluginJava.default],
+				tabWidth: 4
+			});
+
+			const start = document.positionAt(0);
+			const end = document.positionAt(document.getText().length - 1);
+
+			return [vscode.TextEdit.replace(new vscode.Range(start, end), formattedText)];
+		}
+	});
+
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(command);
+	context.subscriptions.push(formatter);
 }
 
 // This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
